8bc3a8200040b9682b700ff5d45ec4d72739c199
Ruafafa <1540588520@qq.com>
2024-05-04 15:20:01 +0000

    add 2 markdown UTC 2024-05-04 Saturday 15:20:01 Updated By Github Actions

---
+++ js/live2d.js
@@ -0,0 +1,6 @@
+if (window.screen.width > 480) {
+    var resourcesPath = 'https://cdn.jsdelivr.net/gh/Ruafafa/live2d_modules@master/';
+    var backImageName = '';
+    var modelDir = ['Murasame_Yukata'];
+    initDefine(resourcesPath, backImageName, modelDir);
+}
